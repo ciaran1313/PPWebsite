b3c6519ce89cae441f72c2b84adbcf5489ae54b4
ciaran1313 <caerfinn@gmail.com>
2018-03-02 11:28:36 -0500

    started trying to make the scrolling to sections more smooth. Couldn't get it to work properly and commented out the code, but left it in for whenever I get around to doing more work on it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
+function scrollToElementById(elementID){
+  var target = $(document.getElementById(elementID)).offset().top;
+  var position = $(window).scrollTop();
+  var direction = target <= scrollTop ? (+1) : (-1);
+  function increment(){
+    position += direction;
+    $(window).scrollTop(position);
+    setTimeout(increment, 10);
+  }
+}
+
 $(document).scroll(resetNavbarStyle);
 
 $(window).resize(function(){
@@ -14,4 +25,7 @@ $(document).ready(function(){
   });
   $("#home-link").click(setNavbarStyle0);
   $("#social-media-links a").attr("target", "_blank");
+  /*$("#links li a").each(function(){
+    $(this).attr("href",`javascript: scrollToElementById( ${ $(this).attr("href").substr(1) } )`);
+  });*/
 });
